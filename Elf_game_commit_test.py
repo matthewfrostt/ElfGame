94106d303931d50031f7016b1e0a4f1c3f0a4e5f
lucascalves04 <75144114+lucascalves04@users.noreply.github.com>
2020-11-27 21:36:09 +0000

    Update Elf_game_commit_test.py

--- Elf_game_commit_test.py
+++ Elf_game_commit_test.py
@@ -3,7 +3,7 @@ import random
 dice_roll = random.randint(0, 7)
 
 if (dice_roll % 2) == 0:
-    print("Thiss rolled an even number")
+    print("The dice rolled an even number!")
 else:
-    print("this was not an odd")
+    print("The dice rolled an odd number!")
 
