6926285f24a7901823046f5f471869d1adc4a73e
lucascalves04 <75144114+lucascalves04@users.noreply.github.com>
2020-11-28 19:16:59 +0000

    Update elf_profmade.py

--- code/elf_profmade.py
+++ code/elf_profmade.py
@@ -12,11 +12,9 @@ mountains = 50
 elf_am = 10
 print("Day 1!, Current Elfs = {}".format(elf_am))
 print("You currently have ${} in your balance!".format(cash))
-elf_s = input("How many Elfs would you like to send to work today?: ")
+elf_s = int(input("How many Elfs would you like to send to work today?: "))
 woodsp = elf_s * woods
 choice = input("Pick a destination to send the Elfs to work: 1 - Woods, 2 - Forest, 3 - Mountains: ")
 if choice == "1":
-    slowprint("You have sent {} Elf/Elfs to the Woods and made ${} !".format(elf_s,woodsp))
-    
-#Notes - Need to fix issue, output e.g. if 1 elf is sent to woods, instead of it outputting made $10 it outputs $11111111    
+    slowprint("You have sent {} Elf/Elfs to the Woods and made ${} !".format(elf_s,woodsp)) 
     
